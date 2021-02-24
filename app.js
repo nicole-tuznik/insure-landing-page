c1effc307de9cfd6d74d4804d7ca7daac4ed48d4
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-02-24 15:56:34 -0500

    Added hover effects

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const hamburger = document.querySelector(".nav__hamburger");
 hamburger.addEventListener("click", function () {
   navList.classList.toggle("show-menu");
 
+  if (navList.classList.contains("show-menu")) {
+    hamburger.src = "/images/icon-close.svg";
+  } else {
+    hamburger.src = "/images/icon-hamburger.svg";
+  }
+
   document.body.classList.toggle("no-scroll");
 });
